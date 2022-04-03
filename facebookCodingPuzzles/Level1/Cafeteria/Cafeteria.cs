7258993c5f037a090952ecf5cf0dcbdd1d1f17e4
Gabor Hodi <gaborhodi@yahoo.com>
2022-04-03 20:47:37 +0200

    Add comments and unit test

--- facebookCodingPuzzles/Level1/Cafeteria/Cafeteria.cs
+++ facebookCodingPuzzles/Level1/Cafeteria/Cafeteria.cs
@@ -1,12 +1,15 @@
 namespace Level1
 {
     internal static class Cafeteria {
-        public static long getMaxAdditionalDinersCount(long N, long K, int M, long[] S) {
+        public static long GetMaxAdditionalDinersCount(long N, long K, int M, long[] S) {
+            // the space needed between two existing diner in order to be able to seat one more diner
             var space = 1 + 2*K;
             var result = 0L;
             var diff = 0L;
+
+            // making S ordered so the for loop can iterate it through correctly
             S = S.OrderBy(s => s).ToArray();
-            for(int i = 1; i<=S.Length-1; i++)
+            for(var i = 1; i<=S.Length-1; i++)
             {
                 diff = S[i] - S[i-1] - 1;
                 if(diff >= space)
@@ -16,10 +19,12 @@ namespace Level1
                     result += diff / (K+1);
                 }
             }
+
+            // add diner(s) to the left of the first diner
             diff = S[0] - 1;
             result += diff / (K+1);
 
-            // when   (i==S.Length)
+            // add diner(s) to the right of the last diner
             diff = N - S[S.Length-1];
             result += diff / (K+1);
 
--- facebookCodingPuzzles/README.md
+++ facebookCodingPuzzles/README.md
@@ -1,3 +1,7 @@
-#### These challenges are from the [Facebook Recruiting site](https://www.facebookrecruiting.com/portal/coding_puzzles/).
+### These challenges are from the [Facebook Recruiting site](https://www.facebookrecruiting.com/portal/coding_puzzles/).
 
  One needs to register in order to have access and to be able to run code against the tests
+
+---
+
+ There are 5 leves of challenges, warmup, Level 1 ... Level 4. The folder structure follows that categorisation.
