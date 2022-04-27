94582e5a5134ce054d0fadf26251a0c1f8ea5060
Gabor Hodi <gaborhodi@yahoo.com>
2022-04-27 09:46:02 +0200

    Add comments and tests to the 'Hops' exercise

--- facebookCodingPuzzles/level2/hops/hops.js
+++ facebookCodingPuzzles/level2/hops/hops.js
@@ -7,7 +7,7 @@
  * @return {number}
  */
  function getSecondsRequired(N, F, P) {
-    // there is only one frog
+    // when there is only one frog, it needs to jump through all lily pads
     if (F === 1) return N - P[0];
 
     // all lily pads are occupied
@@ -16,12 +16,15 @@
     let result = 0;
     P.sort((a, b) => a-b);
 
-    for(let i=1; i< P.length; i++)
+    // all free lily pads should be jumped on only once
+    // so it is enough to calculate the free lily pads
+    for (let i=1; i< P.length; i++)
     {
         let diff = P[i] - P[i-1] - 1;
         result += diff;
     }
 
+    // calculate the free lily pads ahead of the leading frog
     result += N - P[P.length-1] - 1;
 
     return result + F;
--- facebookCodingPuzzles/level2/hops/hops.test.js
+++ facebookCodingPuzzles/level2/hops/hops.test.js
@@ -13,3 +13,11 @@ test("Example 2 returns 4", () => {
 test("Custom test returns 14", () => {
     expect(getSecondsRequired(13, 5, [11, 9, 2, 5, 10])).toBe(11);
 })
+
+test("All lily pads are occupied returns the number of the frogs", () => {
+    expect(getSecondsRequired(3, 2, [1,2])).toBe(2);
+})
+
+test("Alone frog returns the number of lily pads ahead of it", () => {
+    expect(getSecondsRequired(4, 1, [1])).toBe(3);
+})
